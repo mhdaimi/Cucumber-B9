e987011a2f6a42ccd0b5a4da10982a81c72162b7
Mohammad Daimi <mhdaimi@hotmail.com>
2021-06-20 15:59:52 +0530

    change folder in features

--- src/test/java/stepDefinition/TestRunner.java
+++ src/test/java/stepDefinition/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
-		features = {"src/test/resources/features/AmazonSearch.feature"},
+		features = {"src/test/resources/features/"},
 		glue = "stepDefinition",
 		monochrome = true,
 		plugin = {"pretty", "summary", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
